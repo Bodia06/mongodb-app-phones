69bb21f2fe525cacb9303f5627eb734931adb73b
Bohdan <bohdan.verbetskyi@gmail.com>
2025-11-20 13:03:20 +0100

    Add server

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const PORT = process.env.PORT || 5001;
+const HOST = process.env.HOST || 'localhost';
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(PORT, HOST, () => {
+  console.log(`Server is running at http://${HOST}:${PORT}`);
+});
