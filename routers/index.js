cae63b0ddbd508818cb42ff72c7c0cccc006272f
Bohdan <bohdan.verbetskyi@gmail.com>
2025-11-26 10:54:58 +0100

    Delete soame link

--- routers/index.js
+++ routers/index.js
@@ -1,12 +1,10 @@
 const { Router } = require('express');
 const phonesRouter = require('./phonesRouter');
-const phoneRouter = require('./phoneRouter');
 const usersRouter = require('./usersRouter');
 
 const router = Router();
 
 router.use('/phones', phonesRouter);
-router.use('/phone', phoneRouter);
 router.use('/users', usersRouter);
 
 module.exports = router;
